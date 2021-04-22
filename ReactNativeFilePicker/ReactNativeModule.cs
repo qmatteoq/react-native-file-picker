ee9f415cf9ec5cce2116844ced023d52deb4e599
Matteo Pagani <matteo.pagani@microsoft.com>
2021-04-22 18:39:31 +0200

    Add project files.

---
+++ ReactNativeFilePicker/ReactNativeModule.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.ReactNative;
+using Microsoft.ReactNative.Managed;
+using Windows.Storage.Pickers;
+
+namespace ReactNativeFilePicker
+{
+    [ReactModule("ReactNativeFilePicker")]
+    internal sealed class ReactNativeModule
+    {
+        // See https://microsoft.github.io/react-native-windows/docs/native-modules for details on writing native modules
+
+        private ReactContext _reactContext;
+
+        [ReactInitializer]
+        public void Initialize(ReactContext reactContext)
+        {
+            _reactContext = reactContext;
+        }
+
+        [ReactMethod("pick")]
+        public async Task<string> PickFile()
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            openPicker.FileTypeFilter.Add("*");
+            var file = await openPicker.PickSingleFileAsync();
+
+            string base64Content = string.Empty;
+
+            var fileStream = await file.OpenReadAsync();
+
+            using (StreamReader reader = new StreamReader(fileStream.AsStream()))
+            {
+                using (var memstream = new MemoryStream())
+                {
+                    reader.BaseStream.CopyTo(memstream);
+                    var bytes = memstream.ToArray();
+                    base64Content = Convert.ToBase64String(bytes);
+                }
+            }
+
+            return base64Content;
+
+        }
+    }
+}
